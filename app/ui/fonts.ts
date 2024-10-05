d22fbe84a9387c346cb01af8375c5abc5fd08819
KeonW <Keontwilliams@gmail.com>
2024-10-04 21:35:44 -0400

    fix for font issue

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,5 +1,6 @@
 import {Inter} from 'next/font/google'; 
 import { Roboto} from 'next/font/google'; 
+import { Lusitana } from 'next/font/google';
 
 export const inter = Inter({ 
   subsets : ['latin']
@@ -8,5 +9,9 @@ export const inter = Inter({
 export const roboto = Roboto({
   weight : "100", 
   subsets : ['latin']
+}); 
+
+export const lusitana = Lusitana({
+  weight : "400"
 }); 
 
